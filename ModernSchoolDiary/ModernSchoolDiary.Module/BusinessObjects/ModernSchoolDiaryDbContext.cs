06145f2fe0d81caf0939b449f9009d7d4ce7ed47
Grigorii <griger1011@gmail.com>
2026-06-02 13:41:18 +0300

    Initial commit

---
+++ ModernSchoolDiary/ModernSchoolDiary.Module/BusinessObjects/ModernSchoolDiaryDbContext.cs
@@ -0,0 +1,97 @@
+﻿using DevExpress.ExpressApp.Design;
+using DevExpress.ExpressApp.EFCore.DesignTime;
+using DevExpress.ExpressApp.EFCore.Updating;
+using DevExpress.Persistent.BaseImpl.EF;
+using DevExpress.Persistent.BaseImpl.EF.PermissionPolicy;
+using DevExpress.Persistent.BaseImpl.EF.StateMachine;
+using DevExpress.Persistent.BaseImpl.EFCore.AuditTrail;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace ModernSchoolDiary.Module.BusinessObjects
+{
+    [TypesInfoInitializer(typeof(DbContextTypesInfoInitializer<ModernSchoolDiaryEFCoreDbContext>))]
+    public class ModernSchoolDiaryEFCoreDbContext : DbContext
+    {
+        public ModernSchoolDiaryEFCoreDbContext(DbContextOptions<ModernSchoolDiaryEFCoreDbContext> options) : base(options)
+        {
+        }
+        //public DbSet<ModuleInfo> ModulesInfo { get; set; }
+        public DbSet<ModelDifference> ModelDifferences { get; set; }
+        public DbSet<ModelDifferenceAspect> ModelDifferenceAspects { get; set; }
+        public DbSet<PermissionPolicyRole> Roles { get; set; }
+        public DbSet<ModernSchoolDiary.Module.BusinessObjects.ApplicationUser> Users { get; set; }
+        public DbSet<ModernSchoolDiary.Module.BusinessObjects.ApplicationUserLoginInfo> UserLoginsInfo { get; set; }
+        public DbSet<FileData> FileData { get; set; }
+        public DbSet<ReportDataV2> ReportDataV2 { get; set; }
+        public DbSet<StateMachine> StateMachines { get; set; }
+        public DbSet<StateMachineState> StateMachineStates { get; set; }
+        public DbSet<StateMachineTransition> StateMachineTransitions { get; set; }
+        public DbSet<StateMachineAppearance> StateMachineAppearances { get; set; }
+        public DbSet<DashboardData> DashboardData { get; set; }
+        public DbSet<AuditDataItemPersistent> AuditData { get; set; }
+        public DbSet<AuditEFCoreWeakReference> AuditEFCoreWeakReferences { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.UseDeferredDeletion(this);
+            modelBuilder.UseOptimisticLock();
+            modelBuilder.SetOneToManyAssociationDeleteBehavior(DeleteBehavior.SetNull, DeleteBehavior.Cascade);
+            modelBuilder.HasChangeTrackingStrategy(ChangeTrackingStrategy.ChangingAndChangedNotificationsWithOriginalValues);
+            modelBuilder.UsePropertyAccessMode(PropertyAccessMode.PreferFieldDuringConstruction);
+            modelBuilder.Entity<ModernSchoolDiary.Module.BusinessObjects.ApplicationUserLoginInfo>(b =>
+            {
+                b.HasIndex(nameof(DevExpress.ExpressApp.Security.ISecurityUserLoginInfo.LoginProviderName), nameof(DevExpress.ExpressApp.Security.ISecurityUserLoginInfo.ProviderUserKey)).IsUnique();
+            });
+            modelBuilder.Entity<AuditEFCoreWeakReference>()
+                .HasMany(p => p.AuditItems)
+                .WithOne(p => p.AuditedObject);
+            modelBuilder.Entity<AuditEFCoreWeakReference>()
+                .HasMany(p => p.OldItems)
+                .WithOne(p => p.OldObject);
+            modelBuilder.Entity<AuditEFCoreWeakReference>()
+                .HasMany(p => p.NewItems)
+                .WithOne(p => p.NewObject);
+            modelBuilder.Entity<AuditEFCoreWeakReference>()
+                .HasMany(p => p.UserItems)
+                .WithOne(p => p.UserObject);
+            modelBuilder.Entity<StateMachine>()
+                .HasMany(t => t.States)
+                .WithOne(t => t.StateMachine)
+                .OnDelete(DeleteBehavior.Cascade);
+            modelBuilder.Entity<ModelDifference>()
+                .HasMany(t => t.Aspects)
+                .WithOne(t => t.Owner)
+                .OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+
+    public class ModernSchoolDiaryAuditingDbContext : DbContext
+    {
+        public ModernSchoolDiaryAuditingDbContext(DbContextOptions<ModernSchoolDiaryAuditingDbContext> options) : base(options)
+        {
+        }
+        public DbSet<AuditDataItemPersistent> AuditData { get; set; }
+        public DbSet<AuditEFCoreWeakReference> AuditEFCoreWeakReferences { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.UseDeferredDeletion(this);
+            modelBuilder.HasChangeTrackingStrategy(ChangeTrackingStrategy.ChangingAndChangedNotificationsWithOriginalValues);
+            modelBuilder.Entity<AuditEFCoreWeakReference>()
+                .HasMany(p => p.AuditItems)
+                .WithOne(p => p.AuditedObject);
+            modelBuilder.Entity<AuditEFCoreWeakReference>()
+                .HasMany(p => p.OldItems)
+                .WithOne(p => p.OldObject);
+            modelBuilder.Entity<AuditEFCoreWeakReference>()
+                .HasMany(p => p.NewItems)
+                .WithOne(p => p.NewObject);
+            modelBuilder.Entity<AuditEFCoreWeakReference>()
+                .HasMany(p => p.UserItems)
+                .WithOne(p => p.UserObject);
+        }
+    }
+}
---
+++ ModernSchoolDiary/ModernSchoolDiary.Win/WinApplication.cs
@@ -0,0 +1,67 @@
+﻿using System.ComponentModel;
+using System.Data.Common;
+using DevExpress.EntityFrameworkCore.Security;
+using DevExpress.ExpressApp;
+using DevExpress.ExpressApp.ApplicationBuilder;
+using DevExpress.ExpressApp.EFCore;
+using DevExpress.ExpressApp.Security;
+using DevExpress.ExpressApp.Security.ClientServer;
+using DevExpress.ExpressApp.Updating;
+using DevExpress.ExpressApp.Win;
+using DevExpress.ExpressApp.Win.Utils;
+using DevExpress.Persistent.BaseImpl.EFCore.AuditTrail;
+using Microsoft.EntityFrameworkCore;
+using ModernSchoolDiary.Module;
+using ModernSchoolDiary.Module.BusinessObjects;
+
+namespace ModernSchoolDiary.Win
+{
+    // For more typical usage scenarios, be sure to check out https://docs.devexpress.com/eXpressAppFramework/DevExpress.ExpressApp.Win.WinApplication._members
+    public class ModernSchoolDiaryWindowsFormsApplication : WinApplication
+    {
+        public ModernSchoolDiaryWindowsFormsApplication()
+        {
+            SplashScreen = new DXSplashScreen(typeof(XafSplashScreen), new DefaultOverlayFormOptions());
+            ApplicationName = "ModernSchoolDiary";
+            CheckCompatibilityType = DevExpress.ExpressApp.CheckCompatibilityType.DatabaseSchema;
+            UseOldTemplates = false;
+            DatabaseVersionMismatch += ModernSchoolDiaryWindowsFormsApplication_DatabaseVersionMismatch;
+            CustomizeLanguagesList += ModernSchoolDiaryWindowsFormsApplication_CustomizeLanguagesList;
+        }
+        void ModernSchoolDiaryWindowsFormsApplication_CustomizeLanguagesList(object sender, CustomizeLanguagesListEventArgs e)
+        {
+            string userLanguageName = System.Threading.Thread.CurrentThread.CurrentUICulture.Name;
+            if (userLanguageName != "en-US" && e.Languages.IndexOf(userLanguageName) == -1)
+            {
+                e.Languages.Add(userLanguageName);
+            }
+        }
+        void ModernSchoolDiaryWindowsFormsApplication_DatabaseVersionMismatch(object sender, DevExpress.ExpressApp.DatabaseVersionMismatchEventArgs e)
+        {
+#if EASYTEST
+            e.Updater.Update();
+            e.Handled = true;
+#else
+            if (System.Diagnostics.Debugger.IsAttached)
+            {
+                e.Updater.Update();
+                e.Handled = true;
+            }
+            else
+            {
+                string message = "The application cannot connect to the specified database, " +
+                    "because the database doesn't exist, its version is older " +
+                    "than that of the application or its schema does not match " +
+                    "the ORM data model structure. To avoid this error, use one " +
+                    "of the solutions from the https://www.devexpress.com/kb=T367835 KB Article.";
+
+                if (e.CompatibilityError != null && e.CompatibilityError.Exception != null)
+                {
+                    message += "\r\n\r\nInner exception: " + e.CompatibilityError.Exception.Message;
+                }
+                throw new InvalidOperationException(message);
+            }
+#endif
+        }
+    }
+}
